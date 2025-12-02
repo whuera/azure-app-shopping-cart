1f0916365976d7e5bff316e5d7d04d1af5c76df0
whuera <whuera@gmail.com>
2025-12-01 19:51:04 -0500

    update files

---
+++ Tests/src/test/ShoppingCartItemsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.app.mobilpymes.shoppingcart;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartItemsApplicationTests {
+/*
+	@Test
+	void contextLoads() {
+	}
+ */
+}
--- src/main/java/com/app/mobilpymes/shoppingcart/services/CartItemsService.java
+++ src/main/java/com/app/mobilpymes/shoppingcart/services/CartItemsService.java
@@ -5,8 +5,20 @@ import com.app.mobilpymes.shoppingcart.entity.Customer;
 
 import java.util.List;
 
-public
-interface CartItemsService {
-    public
-    List < CartItems > listCartItems (Customer customer);
+/**
+ * Service interface for managing cart items.
+ * <p>
+ * This service provides methods to retrieve and manipulate items within a
+ * shopping cart.
+ * </p>
+ */
+public interface CartItemsService {
+
+    /**
+     * Retrieves a list of cart items associated with a specific customer.
+     *
+     * @param customer The customer whose cart items are to be retrieved.
+     * @return A list of {@link CartItems} belonging to the customer.
+     */
+    public List<CartItems> listCartItems(Customer customer);
 }
